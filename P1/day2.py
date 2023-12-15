9e6b328977ab154cdb0b7738f21623b73109160d
ranjit681 <ranjithacharya681@gmail.com>
2023-12-16 01:05:06 +0530

    Done with code

---
+++ P1/day2.py
@@ -0,0 +1,8 @@
+# List of numbers
+numbers = [4, 2, 7, 1, 9, 5]
+
+# Sorting the numbers in ascending order
+sorted_numbers = sorted(numbers)
+
+print("Original list:", numbers)
+print("Sorted list:", sorted_numbers)
