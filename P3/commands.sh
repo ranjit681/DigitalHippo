d4c6921e6ab043907147458633dac8e17e3ac196
ranjit681 <ranjithacharya681@gmail.com>
2023-12-17 17:31:23 +0530

    Restructured form validation code, reducing complexity and improving readability

--- P3/commands.sh
+++ P3/commands.sh
@@ -4,3 +4,5 @@ Restructured form validation code, reducing complexity and improving readability
 Jenkins CI/CD Pipeline - SonarQube, Docker, Github Webhooks on AWS
 
 https://www.youtube.com/watch?v=361bfIvXMBI
+
+Push the code into github
